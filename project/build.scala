7fb54b72942cc8ee8ea7ebb354faeb6398933c75
Rodrigo Lazoti <rlazoti@corp.hotwords.com>
2015-01-07 14:59:21 -0200

    add sbt-release plugin

--- project/build.scala
+++ project/build.scala
@@ -1,21 +1,46 @@
 import sbt._
 import sbt.Keys._
 import Dependencies._
+import sbtrelease._
+import sbtrelease.ReleaseStep
+import sbtrelease.ReleasePlugin._
+import sbtrelease.ReleasePlugin.ReleaseKeys._
+import sbtrelease.ReleaseStateTransformations._
 
 object AppBuilder extends Build {
 
-  val appName     = "finagle-metrics"
+  val appName = "finagle-metrics"
+
   val appSettings = Seq(
     name          := appName,
     organization  := "com.github.rlazoti",
-    version       := "0.0.1-SNAPSHOT",
     scalaVersion  := "2.11.4",
     unmanagedSourceDirectories in Compile <<= (scalaSource in Compile)(Seq(_)),
     unmanagedSourceDirectories in Test    <<= (scalaSource in Test)(Seq(_))
   )
 
+  // sbt-release settings
+  val appReleaseSettings = releaseSettings ++ Seq(
+    tagName        <<= (version in ThisBuild) map (v => "version-" + v),
+    tagComment     <<= (version in ThisBuild) map (v => "[BUILD] Release %s" format v),
+    commitMessage  <<= (version in ThisBuild) map (v => "[BUILD] Set version to %s" format v),
+    releaseProcess <<= thisProjectRef apply { ref =>
+      Seq[ReleaseStep](
+        checkSnapshotDependencies,
+        inquireVersions,
+        runTest,
+        setReleaseVersion,
+        commitReleaseVersion,
+        tagRelease,
+        setNextVersion,
+        commitNextVersion
+      )
+    }
+  )
+
   lazy val app = Project(appName, file("."))
     .settings(appSettings: _*)
+    .settings(appReleaseSettings: _*)
     .settings(resolvers ++= appDependencyResolvers)
     .settings(libraryDependencies ++= appDependencies)
 
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,3 +1,5 @@
 resolvers += Resolver.sonatypeRepo("snapshots")
 
+addSbtPlugin("com.github.gseitz" % "sbt-release" % "0.8.5")
+
 addSbtPlugin("org.ensime" % "ensime-sbt" % "0.1.5-SNAPSHOT")
